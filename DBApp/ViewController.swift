aeaff4ac6794a7ecd9f80322ce4d605ee76433a4
davidbudac <davidbudac@Davids-MacBook-Pro.local>
2015-09-19 13:24:11 +0200

    Initial commit

---
+++ DBApp/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DBApp
+//
+//  Created by David Budac on 19/09/15.
+//  Copyright (c) 2015 David Budac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
